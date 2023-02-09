c4b96948b138cff23e2a19782ed61e7f4fb3b006
Kishor Pawar <39489942+kspawar@users.noreply.github.com>
2023-02-09 13:01:41 +0530

    Update Problem.js

--- Problem.js
+++ Problem.js
@@ -1,1 +1,31 @@
+// Getting input via STDIN
+const readline = require("readline");
 
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+    let Arr = userInput[1].split(" ");
+    let a = parseInt(userInput[0]);
+    let b = parseInt(Arr[0]);
+    let c = parseInt(Arr[1]);
+
+    if(( a > b && a < c) || ( a > c && a < b ))
+    {
+        console.log('yes');
+    }
+    else
+    {
+        console.log('no');
+    }
+  //end-here
+});
